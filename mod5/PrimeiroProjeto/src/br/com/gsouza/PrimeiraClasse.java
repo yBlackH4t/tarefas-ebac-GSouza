237b2b8f24e0d96406341f8e58d6ec971e841e58
yBlackH4t <gui_pinheirosouza@hotmail.com>
2025-08-22 11:28:31 -0300

    Upando o Primeiro Projeto - ProjetoBASE

---
+++ mod5/PrimeiroProjeto/src/br/com/gsouza/PrimeiraClasse.java
@@ -0,0 +1,11 @@
+package br.com.gsouza;
+
+public class PrimeiraClasse {
+    public static void main(String[] args) {
+
+        String name = "Guilherme";
+
+        System.out.println("Hello, " + name + "!");
+
+    }
+}
