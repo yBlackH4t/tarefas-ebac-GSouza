e726d6607b197f3dc2c23679c1e61218093dfc8f
yBlackH4t <gui_pinheirosouza@hotmail.com>
2025-08-24 16:02:01 -0300

    Aulas, MOD8 concluida!

---
+++ mod8/Exemplos.java
@@ -0,0 +1,32 @@
+public class Exemplos {
+    private int inteiro = 1000000000;
+
+    private long inteiroMelhorLong = 1000000000;
+
+    private double valorDecimal = 12.0f;
+
+    private float valorDecimalF = 10.2f;
+
+    private boolean logico = false;
+
+    private String texto;
+
+    private short valorMenor = 2024;
+
+    private byte bi = 02;
+
+    private char caractere = 'A';
+
+
+    public String retornarTexto(){
+        return "oi";
+    }
+
+    public int retornarInteiro(){
+        int novoNum = 90;
+        return 1000;
+    }
+
+
+
+}
---
+++ mod8/Teste.java
@@ -0,0 +1,13 @@
+public class Teste {
+    public static void main(String[] args) {
+
+        Exemplos exemplosMetodos = new Exemplos();
+
+        System.out.println(exemplosMetodos.retornarTexto());
+        System.out.println(exemplosMetodos.retornarInteiro());
+
+
+
+
+    }
+}
