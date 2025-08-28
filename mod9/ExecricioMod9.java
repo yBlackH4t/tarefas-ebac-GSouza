99015a5fb229ace953cbe1001d02734eae4963bc
yBlackH4t <gui_pinheirosouza@hotmail.com>
2025-08-28 10:53:48 -0300

    Exercise MOD9 ok!

---
+++ mod9/ExecricioMod9.java
@@ -0,0 +1,28 @@
+public class ExecricioMod9 {
+    public static void main(String[] args) {
+/*
+        int num1 = 1000000000;
+        System.out.println(num1);
+        short num2 = (short) num1;
+        System.out.println(num2 + "\n----------");
+
+        int numL = 1000000000;
+        System.out.println(numL);
+        long numLong = numL;
+        System.out.println(numLong *= 1000000000);
+*/
+        byte b = 1;
+        short s = b;
+
+        short s1 = 1010;
+        System.out.println(s1);
+        byte b1 = (byte) s1;
+        int i = b1;
+        byte b2 = (byte) i;
+
+        System.out.println(i);
+
+
+
+    }
+}
